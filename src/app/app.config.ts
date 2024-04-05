3fc5a6cea22c5c17447e2eb7e62d8da7cadf193f
SajidAhmed0 <mjsahmed205@gmail.com>
2024-04-05 20:57:15 +0530

    image upload project finshed successfully

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { AngularFireModule } from '@angular/fire/compat';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment.development';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom([
+      provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+      provideFirestore(() => getFirestore())
+    ]),
+    { provide: FIREBASE_OPTIONS, useValue: environment.firebaseConfig }  
+  ]
 };
